f63298c57d2140cbee713cfd5d7ecaccbd06eeaa
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-11-06 10:15:24 -0300

    Add YFI-A and BAL-A (#5)
    * Add YFI-A and BAL-A
    
    * New YFI and BAL osm contracts

--- src/Megapoker.sol
+++ src/Megapoker.sol
@@ -38,6 +38,8 @@ contract MegaPoker {
     OsmLike constant comp = OsmLike(0xBED0879953E633135a48a157718Aa791AC0108E4);
     OsmLike constant link = OsmLike(0x9B0C694C6939b5EA9584e9b61C7815E8d97D9cC7);
     OsmLike constant lrc  = OsmLike(0x9eb923339c24c40Bef2f4AF4961742AA7C23EF3a);
+    OsmLike constant yfi  = OsmLike(0x5F122465bCf86F45922036970Be6DD7F58820214);
+    OsmLike constant bal  = OsmLike(0x3ff860c0F28D69F392543A16A397D0dAe85D16dE);
     SpotLike constant spot = SpotLike(0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3);
 
     function poke() external {
@@ -51,6 +53,8 @@ contract MegaPoker {
         if (comp.pass()) comp.poke();
         if (link.pass()) link.poke();
         if ( lrc.pass())  lrc.poke();
+        if ( yfi.pass())  yfi.poke();
+        if ( bal.pass())  bal.poke();
 
         spot.poke("ETH-A");
         spot.poke("BAT-A");
@@ -63,6 +67,8 @@ contract MegaPoker {
         spot.poke("LINK-A");
         spot.poke("LRC-A");
         spot.poke("ETH-B");
+        spot.poke("YFI-A");
+        spot.poke("BAL-A");
     }
 
     // Use for poking OSMs prior to collateral being added
@@ -77,6 +83,8 @@ contract MegaPoker {
         if (comp.pass()) comp.poke();
         if (link.pass()) link.poke();
         if ( lrc.pass())  lrc.poke();
+        if ( yfi.pass())  yfi.poke();
+        if ( bal.pass())  bal.poke();
 
         spot.poke("ETH-A");
         spot.poke("BAT-A");
