02a376ae9424f7077aa323193c45aef61a461346
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-02-19 21:49:41 -0300

    Update test for latest dapptools

--- test.sh
+++ test.sh
@@ -3,6 +3,4 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
-SOLC_FLAGS="--optimize --optimize-runs 1000" dapp --use solc:0.6.11 build
-
-LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=1000 dapp --use solc:0.6.11 test --rpc-url="$ETH_RPC_URL" -v
