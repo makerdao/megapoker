1c462899347ea69bd6901a0457b9bf859a6741aa
Brian McMichael <brian@brianmcmichael.com>
2021-03-24 17:44:12 -0400

    Add the RegaPoker

---
+++ src/RegaPoker.t.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: AGPL-3.0
+// The RegaPoker
+//
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.6.11;
+
+import "ds-test/test.sol";
+
+import "./RegaPoker.sol";
+
+interface SpellLike {
+    function eta() external view returns (uint256);
+    function done() external view returns (bool);
+    function schedule() external;
+    function cast() external;
+}
+
+interface ChainLogLike {
+    function getAddress(bytes32) external view returns (address);
+}
+
+interface PauseLike {
+    function delay() external view returns (uint256);
+}
+
+interface ChiefLike {
+    function hat() external view returns (address);
+    function lift(address) external;
+    function lock(uint256) external;
+    function vote(address[] calldata) external;
+}
+
+interface TokenLike {
+    function approve(address, uint256) external;
+}
+
+interface Hevm {
+    function warp(uint256) external;
+    function store(address,bytes32,bytes32) external;
+}
+
+contract RegaPokerTest is DSTest {
+    SpellLike    constant spell     = SpellLike(address(0));
+    SpellLike    constant prevSpell = SpellLike(address(0));
+
+    ChainLogLike constant changelog = ChainLogLike(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+
+    PauseLike pause;
+    ChiefLike chief;
+    TokenLike govToken;
+
+    Hevm hevm;
+
+    address regaPoker;
+
+    bytes20 constant CHEAT_CODE = bytes20(uint160(uint256(keccak256('hevm cheat code'))));
+
+    function setUp() public {
+        hevm = Hevm(address(CHEAT_CODE));
+        regaPoker = address(new RegaPoker());
+        pause = PauseLike(changelog.getAddress("MCD_PAUSE"));
+        chief = ChiefLike(changelog.getAddress("MCD_ADM"));
+        govToken = TokenLike(changelog.getAddress("MCD_GOV"));
+    }
+
+    function vote(SpellLike spell_) private {
+        if (chief.hat() != address(spell_)) {
+            hevm.store(
+                address(govToken),
+                keccak256(abi.encode(address(this), uint256(1))),
+                bytes32(uint256(999999999999 ether))
+            );
+            govToken.approve(address(chief), uint256(-1));
+            chief.lock(999999999999 ether);
+
+            assertTrue(!spell_.done());
+
+            address[] memory yays = new address[](1);
+            yays[0] = address(spell_);
+
+            chief.vote(yays);
+            chief.lift(address(spell_));
+        }
+        assertEq(chief.hat(), address(spell_));
+    }
+
+    function schedule(SpellLike spell_) public {
+        if (spell_.eta() == 0) {
+            spell_.schedule();
+        }
+        assertTrue(spell.eta() > 0);
+    }
+
+    function waitAndCast(SpellLike spell_) public {
+        uint256 castTime = now + pause.delay();
+
+        uint256 day = (castTime / 1 days + 3) % 7;
+        if(day >= 5) {
+            castTime += 7 days - day * 86400;
+        }
+
+        uint256 hour = castTime / 1 hours % 24;
+        if (hour >= 21) {
+            castTime += 24 hours - hour * 3600 + 14 hours;
+        } else if (hour < 14) {
+            castTime += 14 hours - hour * 3600;
+        }
+
+        hevm.warp(castTime);
+        spell_.cast();
+    }
+
+    function try_poke() internal returns (bool ok) {
+        (ok,) = regaPoker.call(abi.encodeWithSignature("poke()"));
+    }
+
+    function try_pokeTemp() internal returns (bool ok) {
+        (ok,) = regaPoker.call(abi.encodeWithSignature("pokeTemp()"));
+    }
+
+    function test_poke() public {
+        if (address(prevSpell) != address(0) && !prevSpell.done()) {
+            vote(prevSpell);
+            schedule(prevSpell);
+            waitAndCast(prevSpell);
+        }
+        if (address(spell) != address(0) && !spell.done()) {
+            vote(spell);
+            schedule(spell);
+            assertTrue(try_pokeTemp());
+            assertTrue(!try_poke());
+            waitAndCast(spell);
+        }
+        RegaPoker(regaPoker).refresh();
+        assertTrue(try_pokeTemp());
+        assertTrue(try_poke());
+    }
+}
