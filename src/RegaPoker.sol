0869494c4e1cef2b66f909802a6164b73502beac
Brian McMichael <brian@brianmcmichael.com>
2021-03-25 01:09:29 -0400

    Cleanup and revert to OSMs

--- src/RegaPoker.sol
+++ src/RegaPoker.sol
@@ -33,7 +33,7 @@ interface OSMLike {
 contract RegaPoker {
 
     string   constant ssig = "poke(bytes32)";
-    string   constant msig = "poke()";
+    string   constant osig = "poke()";
     string   constant rsig = "src()";
 
     Chainlog constant  cl = Chainlog(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
@@ -41,53 +41,29 @@ contract RegaPoker {
     IlkReg   immutable ir;
 
     bytes32[] public ilks;
-    address[] public meds;
+    address[] public osms;
 
     constructor() public {
         ir = IlkReg(cl.getAddress("ILK_REGISTRY"));
         spot = cl.getAddress("MCD_SPOT");
     }
 
-    function try_mpoke(address _med) internal returns (bool ok){
-        (ok,) = _med.call(abi.encodeWithSignature(msig));
-    }
-
-    function try_spoke(bytes32 _ilk) internal returns (bool ok) {
-        (ok,) = spot.call(abi.encodeWithSignature(ssig, _ilk));
-    }
-
-    function try_src(address _osm) internal returns (address src) {
-        (bool _ok, bytes memory _src) = _osm.call(abi.encodeWithSignature(rsig));
-        src = (_ok) ? bytesToAddress(_src) : address(0);
-    }
-
-    function bytesToBytes20(bytes memory _b) internal pure returns (bytes20 _result) {
-        assembly {
-            _result := mload(add(_b, 44))
-        }
-    }
-
-    function bytesToAddress(bytes memory _b) internal pure returns (address) {
-        return address(bytesToBytes20(_b));
-    }
-
     function refresh() external {
         bytes32[] memory _ilks = ilks = ir.list();
-        address[] memory _meds = new address[](_ilks.length);
+        address[] memory _osms = new address[](_ilks.length);
         for (uint256 i = 0; i < _ilks.length; i++) {
-            _meds[i] = try_src(ir.pip(_ilks[i]));
+            _osms[i] = ir.pip(_ilks[i]);
         }
-        meds = _meds;
+        osms = _osms;
     }
 
     function poke() external {
         bytes32[] memory _ilks = ilks;
-        address[] memory _meds = meds;
+        address[] memory _osms = osms;
+        bool _ok;
         for (uint256 i = 0; i < _ilks.length; i++) {
-            if (_meds[i] != address(0)) {
-                try_mpoke(_meds[i]);
-            }
-            try_spoke(_ilks[i]);
+            (_ok,) = _osms[i].call(abi.encodeWithSignature(osig));
+            (_ok,) = spot.call(abi.encodeWithSignature(ssig, _ilks[i]));
         }
     }
 
