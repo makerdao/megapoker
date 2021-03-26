a8ac2bdafb7f7c149e9a1e52682bba48c21e3213
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-03-26 09:56:16 -0300

    pragma ^0.6.12

--- src/MegaPoker.sol
+++ src/MegaPoker.sol
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.6.11;
+pragma solidity ^0.6.12;
 
 contract Addresses {
     address constant eth          = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
