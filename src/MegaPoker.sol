cfc73abf177010f31e840cbd92512e67c84b6299
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-04-06 13:06:17 -0300

    More efficient MegaPoker (#17)
    * More efficient MegaPoker
    
    * pragma ^0.6.12
    
    * Renaming

--- src/MegaPoker.sol
+++ src/MegaPoker.sol
@@ -16,106 +16,94 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.6.11;
+pragma solidity ^0.6.12;
 
-interface OsmLike {
-    function poke() external;
-    function pass() external view returns (bool);
+contract PokingAddresses {
+    // OSMs and Spotter addresses
+    address constant eth          = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
+    address constant bat          = 0xB4eb54AF9Cc7882DF0121d26c5b97E802915ABe6;
+    address constant btc          = 0xf185d0682d50819263941e5f4EacC763CC5C6C42;
+    address constant knc          = 0xf36B79BD4C0904A5F350F1e4f776B81208c13069;
+    address constant zrx          = 0x7382c066801E7Acb2299aC8562847B9883f5CD3c;
+    address constant mana         = 0x8067259EA630601f319FccE477977E55C6078C13;
+    address constant usdt         = 0x7a5918670B0C390aD25f7beE908c1ACc2d314A3C;
+    address constant comp         = 0xBED0879953E633135a48a157718Aa791AC0108E4;
+    address constant link         = 0x9B0C694C6939b5EA9584e9b61C7815E8d97D9cC7;
+    address constant lrc          = 0x9eb923339c24c40Bef2f4AF4961742AA7C23EF3a;
+    address constant yfi          = 0x5F122465bCf86F45922036970Be6DD7F58820214;
+    address constant bal          = 0x3ff860c0F28D69F392543A16A397D0dAe85D16dE;
+    address constant uni          = 0xf363c7e351C96b910b92b45d34190650df4aE8e7;
+    address constant aave         = 0x8Df8f06DC2dE0434db40dcBb32a82A104218754c;
+    address constant univ2daieth  = 0x87ecBd742cEB40928E6cDE77B2f0b5CFa3342A09;
+    address constant univ2wbtceth = 0x771338D5B31754b25D2eb03Cea676877562Dec26;
+    address constant univ2usdceth = 0xECB03Fec701B93DC06d19B4639AA8b5a838472BE;
+    address constant univ2daiusdc = 0x25CD858a00146961611b18441353603191f110A0;
+    address constant univ2ethusdt = 0x9b015AA3e4787dd0df8B43bF2FE6d90fa543E13B;
+    address constant univ2linketh = 0x628009F5F5029544AE84636Ef676D3Cc5755238b;
+    address constant univ2unieth  = 0x8Ce9E9442F2791FC63CD6394cC12F2dE4fbc1D71;
+    address constant univ2wbtcdai = 0x5FB5a346347ACf4FCD3AAb28f5eE518785FB0AD0;
+    address constant univ2aaveeth = 0x8D34DC2c33A6386E96cA562D8478Eaf82305b81a;
+    address constant univ2daiusdt = 0x69562A7812830E6854Ffc50b992c2AA861D5C2d3;
+    address constant spotter      = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
 }
 
-interface SpotLike {
-    function poke(bytes32) external;
-}
-
-contract MegaPoker {
-    OsmLike constant eth          = OsmLike(0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763);
-    OsmLike constant bat          = OsmLike(0xB4eb54AF9Cc7882DF0121d26c5b97E802915ABe6);
-    OsmLike constant btc          = OsmLike(0xf185d0682d50819263941e5f4EacC763CC5C6C42);
-    OsmLike constant knc          = OsmLike(0xf36B79BD4C0904A5F350F1e4f776B81208c13069);
-    OsmLike constant zrx          = OsmLike(0x7382c066801E7Acb2299aC8562847B9883f5CD3c);
-    OsmLike constant mana         = OsmLike(0x8067259EA630601f319FccE477977E55C6078C13);
-    OsmLike constant usdt         = OsmLike(0x7a5918670B0C390aD25f7beE908c1ACc2d314A3C);
-    OsmLike constant comp         = OsmLike(0xBED0879953E633135a48a157718Aa791AC0108E4);
-    OsmLike constant link         = OsmLike(0x9B0C694C6939b5EA9584e9b61C7815E8d97D9cC7);
-    OsmLike constant lrc          = OsmLike(0x9eb923339c24c40Bef2f4AF4961742AA7C23EF3a);
-    OsmLike constant yfi          = OsmLike(0x5F122465bCf86F45922036970Be6DD7F58820214);
-    OsmLike constant bal          = OsmLike(0x3ff860c0F28D69F392543A16A397D0dAe85D16dE);
-    OsmLike constant uni          = OsmLike(0xf363c7e351C96b910b92b45d34190650df4aE8e7);
-    OsmLike constant aave         = OsmLike(0x8Df8f06DC2dE0434db40dcBb32a82A104218754c);
-    OsmLike constant univ2daieth  = OsmLike(0x87ecBd742cEB40928E6cDE77B2f0b5CFa3342A09);
-    OsmLike constant univ2wbtceth = OsmLike(0x771338D5B31754b25D2eb03Cea676877562Dec26);
-    OsmLike constant univ2usdceth = OsmLike(0xECB03Fec701B93DC06d19B4639AA8b5a838472BE);
-    OsmLike constant univ2daiusdc = OsmLike(0x25CD858a00146961611b18441353603191f110A0);
-    OsmLike constant univ2ethusdt = OsmLike(0x9b015AA3e4787dd0df8B43bF2FE6d90fa543E13B);
-    OsmLike constant univ2linketh = OsmLike(0x628009F5F5029544AE84636Ef676D3Cc5755238b);
-    OsmLike constant univ2unieth  = OsmLike(0x8Ce9E9442F2791FC63CD6394cC12F2dE4fbc1D71);
-    OsmLike constant univ2wbtcdai = OsmLike(0x5FB5a346347ACf4FCD3AAb28f5eE518785FB0AD0);
-    OsmLike constant univ2aaveeth = OsmLike(0x8D34DC2c33A6386E96cA562D8478Eaf82305b81a);
-    OsmLike constant univ2daiusdt = OsmLike(0x69562A7812830E6854Ffc50b992c2AA861D5C2d3);
-    SpotLike constant spot        = SpotLike(0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3);
-
-    function process() internal {
-        if (         eth.pass())           eth.poke();
-        if (         bat.pass())           bat.poke();
-        if (         btc.pass())           btc.poke();
-        if (         knc.pass())           knc.poke();
-        if (         zrx.pass())           zrx.poke();
-        if (        mana.pass())          mana.poke();
-        if (        usdt.pass())          usdt.poke();
-        if (        comp.pass())          comp.poke();
-        if (        link.pass())          link.poke();
-        if (         lrc.pass())           lrc.poke();
-        if (         yfi.pass())           yfi.poke();
-        if (         bal.pass())           bal.poke();
-        if (         uni.pass())           uni.poke();
-        if (        aave.pass())          aave.poke();
-        if ( univ2daieth.pass())   univ2daieth.poke();
-        if (univ2wbtceth.pass())  univ2wbtceth.poke();
-        if (univ2usdceth.pass())  univ2usdceth.poke();
-        if (univ2daiusdc.pass())  univ2daiusdc.poke();
-        if (univ2ethusdt.pass())  univ2ethusdt.poke();
-        if (univ2linketh.pass())  univ2linketh.poke();
-        if ( univ2unieth.pass())   univ2unieth.poke();
-        if (univ2wbtcdai.pass())  univ2wbtcdai.poke();
-        if (univ2aaveeth.pass())  univ2aaveeth.poke();
-        if (univ2daiusdt.pass())  univ2daiusdt.poke();
-
-        spot.poke("ETH-A");
-        spot.poke("BAT-A");
-        spot.poke("WBTC-A");
-        spot.poke("KNC-A");
-        spot.poke("ZRX-A");
-        spot.poke("MANA-A");
-        spot.poke("USDT-A");
-        spot.poke("COMP-A");
-        spot.poke("LINK-A");
-        spot.poke("LRC-A");
-        spot.poke("ETH-B");
-        spot.poke("YFI-A");
-        spot.poke("BAL-A");
-        spot.poke("RENBTC-A");
-        spot.poke("UNI-A");
-        spot.poke("AAVE-A");
-        spot.poke("UNIV2DAIETH-A");
-        spot.poke("UNIV2WBTCETH-A");
-        spot.poke("UNIV2USDCETH-A");
-        spot.poke("UNIV2DAIUSDC-A");
-        spot.poke("UNIV2ETHUSDT-A");
-        spot.poke("UNIV2LINKETH-A");
-        spot.poke("UNIV2UNIETH-A");
-        spot.poke("UNIV2WBTCDAI-A");
-        spot.poke("UNIV2AAVEETH-A");
-        spot.poke("UNIV2DAIUSDT-A");
-    }
-
+contract MegaPoker is PokingAddresses {
     function poke() external {
-        process();
+        bool ok;
 
-        spot.poke("ETH-C");
-    }
+        // poke() = 0x18178358
+        (ok,) = eth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = bat.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = btc.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = knc.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = zrx.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = mana.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = usdt.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = comp.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = link.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = lrc.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = yfi.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = bal.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = uni.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = aave.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2daieth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2wbtceth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2usdceth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2daiusdc.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2ethusdt.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2linketh.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2unieth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2wbtcdai.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2aaveeth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = univ2daiusdt.call(abi.encodeWithSelector(0x18178358));
 
-    // Use for poking OSMs prior to collateral being added
-    function pokeTemp() external {
-        process();
+        // poke(bytes32) = 0x1504460f
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("ETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("BAT-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("WBTC-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("KNC-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("ZRX-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("MANA-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("USDT-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("COMP-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("LINK-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("LRC-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("ETH-B")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("YFI-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("BAL-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("RENBTC-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNI-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("AAVE-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2DAIETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2WBTCETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2USDCETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2DAIUSDC-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2ETHUSDT-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2LINKETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2UNIETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2WBTCDAI-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2AAVEETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("UNIV2DAIUSDT-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("ETH-C")));
     }
 }
--- src/MegaPoker.t.sol
+++ src/MegaPoker.t.sol
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.6.11;
+pragma solidity ^0.6.12;
 
 import "ds-test/test.sol";
 
@@ -48,12 +48,26 @@ interface TokenLike {
     function approve(address, uint256) external;
 }
 
+interface OsmLike {
+    function read() external view returns (bytes32);
+}
+
+interface SpotLike {
+    function ilks(bytes32) external view returns (address, uint256);
+    function par() external view returns (uint256);
+    function vat() external view returns (address);
+}
+
+interface VatLike {
+    function ilks(bytes32) external view returns (uint256, uint256, uint256, uint256, uint256);
+}
+
 interface Hevm {
     function warp(uint256) external;
     function store(address,bytes32,bytes32) external;
 }
 
-contract MegaPokerTest is DSTest {
+contract MegaPokerTest is DSTest, PokingAddresses {
     SpellLike    constant spell     = SpellLike(0x4145774D007C88392118f32E2c31686faCc9486E);
     SpellLike    constant prevSpell = SpellLike(address(0));
 
@@ -65,16 +79,26 @@ contract MegaPokerTest is DSTest {
 
     Hevm hevm;
 
-    address megaPoker;
+    MegaPoker megaPoker;
 
     bytes20 constant CHEAT_CODE = bytes20(uint160(uint256(keccak256('hevm cheat code'))));
 
     function setUp() public {
         hevm = Hevm(address(CHEAT_CODE));
-        megaPoker = address(new MegaPoker());
+        megaPoker = new MegaPoker();
         pause = PauseLike(changelog.getAddress("MCD_PAUSE"));
         chief = ChiefLike(changelog.getAddress("MCD_ADM"));
         govToken = TokenLike(changelog.getAddress("MCD_GOV"));
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    uint256 constant RAY = 10 ** 27;
+
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = mul(x, RAY) / y;
     }
 
     function vote(SpellLike spell_) private {
@@ -124,13 +148,6 @@ contract MegaPokerTest is DSTest {
         spell_.cast();
     }
 
-    function try_poke() internal returns (bool ok) {
-        (ok,) = megaPoker.call(abi.encodeWithSignature("poke()"));
-    }
-
-    function try_pokeTemp() internal returns (bool ok) {
-        (ok,) = megaPoker.call(abi.encodeWithSignature("pokeTemp()"));
-    }
 
     function test_poke() public {
         if (address(prevSpell) != address(0) && !prevSpell.done()) {
@@ -138,14 +155,202 @@ contract MegaPokerTest is DSTest {
             schedule(prevSpell);
             waitAndCast(prevSpell);
         }
-        if (address(spell) != address(0) && !spell.done()) {
-            vote(spell);
-            schedule(spell);
-            assertTrue(try_pokeTemp());
-            assertTrue(!try_poke());
-            waitAndCast(spell);
-        }
-        assertTrue(try_pokeTemp());
-        assertTrue(try_poke());
+
+        // Hacking nxt price to 0x123 (and making it valid)
+        bytes32 hackedValue = 0x0000000000000000000000000000000100000000000000000000000000000123;
+        hevm.store(eth, bytes32(uint256(4)), hackedValue);
+        hevm.store(bat, bytes32(uint256(4)), hackedValue);
+        hevm.store(btc, bytes32(uint256(4)), hackedValue);
+        hevm.store(knc, bytes32(uint256(4)), hackedValue);
+        hevm.store(zrx, bytes32(uint256(4)), hackedValue);
+        hevm.store(mana, bytes32(uint256(4)), hackedValue);
+        hevm.store(usdt, bytes32(uint256(4)), hackedValue);
+        hevm.store(comp, bytes32(uint256(4)), hackedValue);
+        hevm.store(link, bytes32(uint256(4)), hackedValue);
+        hevm.store(lrc, bytes32(uint256(4)), hackedValue);
+        hevm.store(yfi, bytes32(uint256(4)), hackedValue);
+        hevm.store(bal, bytes32(uint256(4)), hackedValue);
+        hevm.store(uni, bytes32(uint256(4)), hackedValue);
+        hevm.store(aave, bytes32(uint256(4)), hackedValue);
+        hevm.store(univ2daieth, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2wbtceth, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2usdceth, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2daiusdc, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2ethusdt, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2linketh, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2unieth, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2wbtcdai, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2aaveeth, bytes32(uint256(7)), hackedValue);
+        hevm.store(univ2daiusdt, bytes32(uint256(7)), hackedValue);
+
+        // Whitelisting tester address
+        hevm.store(eth, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(bat, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(btc, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(knc, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(zrx, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(mana, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(usdt, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(comp, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(link, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(lrc, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(yfi, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(bal, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(uni, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(aave, keccak256(abi.encode(address(this), uint256(5))), bytes32(uint256(1)));
+        hevm.store(univ2daieth, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2wbtceth, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2usdceth, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2daiusdc, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2ethusdt, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2linketh, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2unieth, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2wbtcdai, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2aaveeth, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+        hevm.store(univ2daiusdt, keccak256(abi.encode(address(this), uint256(2))), bytes32(uint256(1)));
+
+        hevm.warp(now + 3601);
+
+        // 0x123
+        hackedValue = 0x0000000000000000000000000000000000000000000000000000000000000123;
+
+        assertTrue(OsmLike(eth).read() != hackedValue);
+        assertTrue(OsmLike(bat).read() != hackedValue);
+        assertTrue(OsmLike(btc).read() != hackedValue);
+        assertTrue(OsmLike(knc).read() != hackedValue);
+        assertTrue(OsmLike(zrx).read() != hackedValue);
+        assertTrue(OsmLike(mana).read() != hackedValue);
+        assertTrue(OsmLike(usdt).read() != hackedValue);
+        assertTrue(OsmLike(comp).read() != hackedValue);
+        assertTrue(OsmLike(link).read() != hackedValue);
+        assertTrue(OsmLike(lrc).read() != hackedValue);
+        assertTrue(OsmLike(yfi).read() != hackedValue);
+        assertTrue(OsmLike(bal).read() != hackedValue);
+        assertTrue(OsmLike(uni).read() != hackedValue);
+        assertTrue(OsmLike(aave).read() != hackedValue);
+        assertTrue(OsmLike(univ2daieth).read() != hackedValue);
+        assertTrue(OsmLike(univ2wbtceth).read() != hackedValue);
+        assertTrue(OsmLike(univ2usdceth).read() != hackedValue);
+        assertTrue(OsmLike(univ2daiusdc).read() != hackedValue);
+        assertTrue(OsmLike(univ2ethusdt).read() != hackedValue);
+        assertTrue(OsmLike(univ2linketh).read() != hackedValue);
+        assertTrue(OsmLike(univ2unieth).read() != hackedValue);
+        assertTrue(OsmLike(univ2wbtcdai).read() != hackedValue);
+        assertTrue(OsmLike(univ2aaveeth).read() != hackedValue);
+        assertTrue(OsmLike(univ2daiusdt).read() != hackedValue);
+
+        megaPoker.poke();
+
+        assertEq(OsmLike(eth).read(), hackedValue);
+        assertEq(OsmLike(bat).read(), hackedValue);
+        assertEq(OsmLike(btc).read(), hackedValue);
+        assertEq(OsmLike(knc).read(), hackedValue);
+        assertEq(OsmLike(zrx).read(), hackedValue);
+        assertEq(OsmLike(mana).read(), hackedValue);
+        assertEq(OsmLike(usdt).read(), hackedValue);
+        assertEq(OsmLike(comp).read(), hackedValue);
+        assertEq(OsmLike(link).read(), hackedValue);
+        assertEq(OsmLike(lrc).read(), hackedValue);
+        assertEq(OsmLike(yfi).read(), hackedValue);
+        assertEq(OsmLike(bal).read(), hackedValue);
+        assertEq(OsmLike(uni).read(), hackedValue);
+        assertEq(OsmLike(aave).read(), hackedValue);
+        assertEq(OsmLike(univ2daieth).read(), hackedValue);
+        assertEq(OsmLike(univ2wbtceth).read(), hackedValue);
+        assertEq(OsmLike(univ2usdceth).read(), hackedValue);
+        assertEq(OsmLike(univ2daiusdc).read(), hackedValue);
+        assertEq(OsmLike(univ2ethusdt).read(), hackedValue);
+        assertEq(OsmLike(univ2linketh).read(), hackedValue);
+        assertEq(OsmLike(univ2unieth).read(), hackedValue);
+        assertEq(OsmLike(univ2wbtcdai).read(), hackedValue);
+        assertEq(OsmLike(univ2aaveeth).read(), hackedValue);
+        assertEq(OsmLike(univ2daiusdt).read(), hackedValue);
+
+        uint256 mat;
+        uint256 spot;
+        uint256 value = rdiv(mul(uint256(hackedValue), 10 ** 9), SpotLike(spotter).par());
+        address vat = SpotLike(spotter).vat();
+
+        (, mat) = SpotLike(spotter).ilks("ETH-A");
+        (,, spot,,) = VatLike(vat).ilks("ETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("BAT-A");
+        (,, spot,,) = VatLike(vat).ilks("BAT-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("WBTC-A");
+        (,, spot,,) = VatLike(vat).ilks("WBTC-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("KNC-A");
+        (,, spot,,) = VatLike(vat).ilks("KNC-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("ZRX-A");
+        (,, spot,,) = VatLike(vat).ilks("ZRX-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("MANA-A");
+        (,, spot,,) = VatLike(vat).ilks("MANA-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("USDT-A");
+        (,, spot,,) = VatLike(vat).ilks("USDT-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("COMP-A");
+        (,, spot,,) = VatLike(vat).ilks("COMP-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("LINK-A");
+        (,, spot,,) = VatLike(vat).ilks("LINK-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("LRC-A");
+        (,, spot,,) = VatLike(vat).ilks("LRC-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("ETH-B");
+        (,, spot,,) = VatLike(vat).ilks("ETH-B");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("YFI-A");
+        (,, spot,,) = VatLike(vat).ilks("YFI-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("BAL-A");
+        (,, spot,,) = VatLike(vat).ilks("BAL-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("RENBTC-A");
+        (,, spot,,) = VatLike(vat).ilks("RENBTC-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNI-A");
+        (,, spot,,) = VatLike(vat).ilks("UNI-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("AAVE-A");
+        (,, spot,,) = VatLike(vat).ilks("AAVE-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2DAIETH-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2DAIETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2WBTCETH-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2WBTCETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2USDCETH-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2USDCETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2DAIUSDC-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2DAIUSDC-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2ETHUSDT-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2ETHUSDT-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2LINKETH-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2LINKETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2UNIETH-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2UNIETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2WBTCDAI-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2WBTCDAI-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2AAVEETH-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2AAVEETH-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("UNIV2DAIUSDT-A");
+        (,, spot,,) = VatLike(vat).ilks("UNIV2DAIUSDT-A");
+        assertEq(spot, rdiv(value, mat));
+        (, mat) = SpotLike(spotter).ilks("ETH-C");
+        (,, spot,,) = VatLike(vat).ilks("ETH-C");
+        assertEq(spot, rdiv(value, mat));
     }
 }
