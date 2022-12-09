8e493eca29bcd3732aa1a521f7369067ffc1cdb2
jar-o <jar@example.com>
2022-12-09 10:22:50 -0700

    Adds GNOUSD

--- src/MegaPoker.sol
+++ src/MegaPoker.sol
@@ -34,6 +34,7 @@ contract PokingAddresses {
     address constant guniv3daiusdc2 = 0xcCBa43231aC6eceBd1278B90c3a44711a00F4e93;
     address constant spotter        = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
     address constant reth           = 0xeE7F0b350aA119b3d05DC733a4621a81972f7D47;
+    address constant gno            = 0xd800ca44fFABecd159c7889c3bf64a217361AEc8;
 }
 
 contract MegaPoker is PokingAddresses {
@@ -55,6 +56,7 @@ contract MegaPoker is PokingAddresses {
         (ok,) = wsteth.call(abi.encodeWithSelector(0x18178358));
         (ok,) = crvv1ethsteth.call(abi.encodeWithSelector(0x18178358));
         (ok,) = reth.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = gno.call(abi.encodeWithSelector(0x18178358));
 
 
         // poke(bytes32) = 0x1504460f
@@ -75,6 +77,7 @@ contract MegaPoker is PokingAddresses {
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("WBTC-B")));
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("WBTC-C")));
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("RETH-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("GNO-A")));
 
 
         // Daily pokes
