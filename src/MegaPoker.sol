a7e16e4247ef02379de3e6ab572a8b99dcfa9a26
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2025-05-05 12:38:08 -0300

    feat: add sky osm and test poking

--- src/MegaPoker.sol
+++ src/MegaPoker.sol
@@ -23,7 +23,7 @@ contract PokingAddresses {
     address constant btc            = 0xf185d0682d50819263941e5f4EacC763CC5C6C42;
     address constant eth            = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
     address constant wsteth         = 0xFe7a2aC0B945f12089aEEB6eCebf4F384D9f043F;
-    address constant mkr            = 0x4F94e33D0D74CfF5Ca0D3a66F1A650628551C56b;
+    address constant sky            = 0xfC3983DE3F7cBe1Ba01084469779470AD0BbeFfa;
 
     address constant guniv3daiusdc1 = 0x7F6d78CC0040c87943a0e0c140De3F77a273bd58;
     address constant guniv3daiusdc2 = 0xcCBa43231aC6eceBd1278B90c3a44711a00F4e93;
@@ -44,7 +44,7 @@ contract MegaPoker is PokingAddresses {
         (ok,) = btc.call(abi.encodeWithSelector(0x18178358));
         (ok,) = eth.call(abi.encodeWithSelector(0x18178358));
         (ok,) = wsteth.call(abi.encodeWithSelector(0x18178358));
-        (ok,) = mkr.call(abi.encodeWithSelector(0x18178358));
+        (ok,) = sky.call(abi.encodeWithSelector(0x18178358));
 
         // poke(bytes32) = 0x1504460f
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("ETH-A")));
@@ -55,7 +55,7 @@ contract MegaPoker is PokingAddresses {
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("WBTC-C")));
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("WSTETH-A")));
         (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("WSTETH-B")));
-        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("LSE-MKR-A")));
+        (ok,) = spotter.call(abi.encodeWithSelector(0x1504460f, bytes32("LSEV2-SKY-A")));
 
         // Daily pokes, i.e. reduced cost pokes
         if (last <= block.timestamp - 1 days) {
